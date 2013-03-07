6b85af7ad599ac4fe8387a52736d2004ea3e9ed6
Geert Bevin <gbevin@uwyn.com>
2013-03-07 17:12:43 +0100

    Selenium tests WIP

--- src/test/java/com/zeroturnaround/rebelanswers/tests/functional/SiteTest.java
+++ src/test/java/com/zeroturnaround/rebelanswers/tests/functional/SiteTest.java
@@ -1,4 +1,36 @@
-package java.com.zeroturnaround.rebelanswers.tests.functional;
+package com.zeroturnaround.rebelanswers.tests.functional;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@RunWith(JUnit4.class)
 public class SiteTest {
+
+  private WebDriver driver;
+
+  @Before
+  public void setUp() throws MalformedURLException {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setBrowserName("chrome");
+    driver = new RemoteWebDriver(new URL("http://10.127.128.2:4444/wd/hub"), capabilities);
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
+    driver = null;
+  }
+
+  @Test
+  public void testIndex() {
+  }
 }
